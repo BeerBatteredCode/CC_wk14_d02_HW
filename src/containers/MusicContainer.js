2c1dee4e29cf6a81438329c574fc2bbdbad8c7d6
Grant Rutherford <contact.grantr@gmail.com>
2019-05-28 22:01:01 +0100

    Music Container added and built

--- src/containers/MusicContainer.js
+++ src/containers/MusicContainer.js
@@ -1,7 +1,36 @@
 import React, {Container} from 'react';
 
 class MusicContainer extends Container {
+    constructor(props){
+        super(props);
+        this.state = {
+            music = [],
+            selectedSong = null
+        };
+        this.selectedSong = this.selectedSong.bind(this);
+    }
 
+    componentDidMount(){
+        const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+        fetch(url)
+        .then(res => res.json())
+        .then((music) => {
+            this.setState({music: music});
+        });
+    }
+
+    render(){
+        return(
+            <div className="music-container">
+                <h3>Music Library</h3>
+                <MusicSelector
+                    music={ this.state.music }
+                    onMusicSelected={ this.state.selectedSong }/>
+                <MusicDetail
+                    music={ this.state.selectedSong }/>
+            </div>
+        )
+    }
 }
 
 export default MusicContainer;
